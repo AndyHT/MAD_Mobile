dcea3da628ed6e1cc4b545b257de1f6580a6c921
Tokkiu <mrgao.ary@gmail.com>
2016-05-04 22:20:27 +0800

    1.增加sever文件 2.删去冗余代码

---
+++ MAD-User/js/sever.js
@@ -0,0 +1,4 @@
+var severUrl='http://121.42.57.59:4000/user';
+//var severUrl='http://localhost:3000/user';
+var userId='userId1';
+var token='dc78501a4076d3f596341f910af8756fc86acfb58893a8d18cebbad5e638c851';
