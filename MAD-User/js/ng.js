a1b0260d7a93d279974f9d121bf2eb9130218cd2
Tokkiu <mrgao.ary@gmail.com>
2016-05-08 21:50:01 +0800

    1.连接提款接口、个人信息接口 2.修改统计页面样式 3.新增相应页面

--- MAD-User/js/ng.js
+++ MAD-User/js/ng.js
@@ -2,7 +2,7 @@ var mad=angular.module('mad',[]);
 mad.controller('mad',['$scope','$http',function($scope,$http){
 	$scope.test="ss";
 	$scope.getads=function(){
-		$http.get(severUrl+'/advert/all/'+userId+"?token="+'dc78501a4076d3f596341f910af8756fc86acfb58893a8d18cebbad5e638c851').then(function(response){
+		$http.get(severUrl+'/advert/all/'+userId+"?token="+token).then(function(response){
 			$scope.ads=response.data.adList;
 			console.log(JSON.stringify(response));
 			console.log($scope.ads)
@@ -13,3 +13,14 @@ mad.controller('mad',['$scope','$http',function($scope,$http){
 	$scope.getads();
 	console.log($scope.ads);
 }]);
+mad.controller('madUser',['$scope','$http',function($scope,$http){
+	$scope.getInfo=function(){
+		$http.get(severUrl+'/account/'+userId+'?token='+token).then(function(response){
+			$scope.info=response.data;
+			console.log(response);
+		},function(error){
+			
+		});
+	};
+	$scope.getInfo();
+}])
