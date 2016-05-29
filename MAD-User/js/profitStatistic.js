923c42c7a7555513dc2d686c4b482e7452fde85e
Tokkiu <mrgao.ary@gmail.com>
2016-05-29 20:57:00 +0800

    1.显示问题修改

--- MAD-User/js/profitStatistic.js
+++ MAD-User/js/profitStatistic.js
@@ -5,7 +5,7 @@ var yearBtn = document.getElementById('yearProfit');
 var hasRecord = true;
 var active_tab = 'day';
 if(localStorage.userId==null) localStorage.userId='18521037217';
-
+getDay();
 ref.child(localStorage.userId).once('value',function(snap){
 	if(snap.val()!=null){
 		ref=ref.child(localStorage.userId);
@@ -56,7 +56,7 @@ function get7days(d)
 		if(d>0) get7days(--d);
 		else{
 			if(active_tab=='day') echartline.setOption(optionline);
-		} 
+		}
 	});
 }
 
@@ -76,7 +76,7 @@ function getThisMonth(d)
 		if(d>0) getThisMonth(--d);
 		else{
 			if(active_tab=='month') echartline.setOption(optionlineMonth);
-		} 
+		}
 	});
 }
 
@@ -98,3 +98,44 @@ function getThisYear(m){
 		}
 	});
 }
+
+function getDay() {
+	var dayIncome;
+	console.log('dayIncome');
+	ref.child(localStorage.userId).child('daily/'+moment().format('YYYY-MM-DD')).once('value',function(snap){
+		if(snap.val()==null){
+			document.getElementById('dayIncome').innerHTML='<h3>'+0+'</h3>'+'MAD币';
+		}else{
+			snap.val().income=dayIncome;
+			document.getElementById('dayIncome').innerHTML='<h3>'+dayIncome+'</h3>'+'MAD币';
+			console.log(dayIncome);
+		}
+	});
+	ref.child(localStorage.userId).child('totalIncome').once('value',function(snap){
+		if(snap.val()==null){
+			//optionline.series[0].data.push(0);
+			document.getElementById('totalIncome').innerHTML='<h3>'+0+'</h3>'+'MAD币';
+		}else{
+			//optionline.series[0].data.push(snap.val().income.toFixed(2));
+			document.getElementById('totalIncome').innerHTML='<h3>'+snap.val()+'</h3>'+'MAD币';
+
+		}
+	});
+}
+
+
+// function getNowFormatDate() {
+// 		var date = new Date();
+// 		var seperator1 = "-";
+// 		var year = date.getFullYear();
+// 		var month = date.getMonth() + 1;
+// 		var strDate = date.getDate();
+// 		if (month >= 1 && month <= 9) {
+// 			month = "0" + month;
+// 		}
+// 		if (strDate >= 0 && strDate <= 9) {
+// 			strDate = "0" + strDate;
+// 		}
+// 		var currentdate = year + seperator1 + month + seperator1 + strDate;
+// 		return currentdate;
+// 	}
